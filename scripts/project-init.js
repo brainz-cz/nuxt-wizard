9c1a4ba575071de9c1da7a507826a5b26fa497e7
Filip <filip.novotny@brainz.cz>
2019-01-04 13:42:18 +0100

    ejs

--- scripts/project-init.js
+++ scripts/project-init.js
@@ -1,62 +1,15 @@
+const ejs = require('ejs')
 const fs = require('fs')
 const { spawn } = require('child_process')
-
-let json = require('../assets/default-package.json')
-
-const addDependency = (name, dev) => {
-  const key = dev ? 'devDependencies' : 'dependencies'
-  json[key][name] = '*'
-}
-
-const addScript = (name, body) => {
-  json.scripts[name] = body
-}
 
 const init = async (options) => {
   const name = options.name
   const modules = options.modules
 
-  json.name = name
-  json.description = name
+  const json = fs.readFileSync('./template/package.json.ejs', 'utf-8')
+  const jsonRender = ejs.render(json, { name: name, modules: modules })
 
-  if(modules.includes('babel')) {
-
-    if(modules.includes('eslint')) {
-      addDependency('babel-eslint', true)
-    }
-  }
-
-  if(modules.includes('gtm')) {
-
-  }
-
-  if(modules.includes('eslint')) {
-    addDependency('eslint', true)
-    addDependency('eslint-loader', true)
-    addDependency('eslint-plugin-vue', true)
-  }
-
-  if(modules.includes('filters')) {
-    addDependency('vue2-filters', true)
-  }
-
-  if(modules.includes('headroom')) {
-    addDependency('vue-headroom')
-  }
-
-  if(modules.includes('heroku-postbuild')) {
-    addScript('heroku-postbuild', 'npm run build')
-  }
-
-  if(modules.includes('i18n')) {
-    addDependency('nuxt-i18n', true)
-  }
-
-  if(modules.includes('sass-boilerplate')) {
-
-  }
-
-  fs.writeFileSync(name + '/package.json', JSON.stringify(json, null, 4))
+  fs.writeFileSync(name + '/package.json', jsonRender)
 
   return await runYarn(name)
 }
